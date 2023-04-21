12adc1830549db522b9e1dddbb46777a36f74ac9
Pedro Castro <aspeddro@gmail.com>
2023-04-20 21:29:32 -0300

    fix ci

--- scripts/moveArtifacts.js
+++ scripts/moveArtifacts.js
@@ -7,10 +7,10 @@ for (const platform of platforms) {
   fs.chmodSync(`binaries-${platform.os}-${platform.arch}`, 0o755);
 }
 
-const other_files = ["cmi_cache.bin", "cmj_cache.bin"];
-for (const other_file of other_files) {
-  fs.renameSync(`cmij-cache/${other_file}`, "lib");
+const cache_files = ["cmi_cache.bin", "cmj_cache.bin"];
+for (const file of cache_files) {
+  fs.renameSync(`cmij-cache/${file}`, `lib/${file}`);
 }
 
-fs.renameSync("lib-ocaml", "lib");
+fs.renameSync("lib-ocaml", "lib/ocaml");
 fs.renameSync("ninja/COPYING", "ninja.COPYING");
